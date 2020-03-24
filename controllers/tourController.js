c4024c7b2be2aebc72edc0ae32f8eac1dd652f7a
MDAkramSiddiqui <you@example.com>
2020-03-24 16:57:12 +0530

    fix(app-Natours): Some refactoring of the code of the previous commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Tour = require('./../models/tourModel');
+const ApiFeatures = require('./../utils/apiFeatures');
 
 class TourController {
 
@@ -12,65 +13,15 @@ class TourController {
   
   async getAllTours(req, res) {
     try{
-
-      // Building Query 
-      // 1. Filtering Query
-      const queryObj = {...req.query};
-      const excludedObj = ['page', 'limit', 'sort', 'fields'];
-      excludedObj.forEach(el => delete queryObj[el]);
-
-      //2.  Advanced Filtering
-      //eg: http://localhost:8000/api/v1/tours?duration[gte]=9&difficulty=medium
-      let queryStr = JSON.stringify(queryObj);
-      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-      queryStr = JSON.parse(queryStr);
-      console.log(queryStr);
-
-      let query = Tour.find(queryStr);
-      // const query = Tour.find();
-
-      //3. Sort
-      if(req.query.sort) {
-        //sorting via multiple parameters in the orders of their appearance
-        //url eg: /tours?sort=price,duration,etc 
-        //query.sort('price duration etc');
-        const sortBy = req.query.sort.split(',').join(' ');
-        query = query.sort(sortBy);
-      }else {
-        //default sorting by -ve createdAt parameter so that the newest appears first
-        query = query.sort('-createdAt'); 
-      }
-
-      //4. Field Limiting
-      if(req.query.fields) {
-        //selecting specific fields from the retrieved data
-        //url eg: /tours?fields=price,duration,etc 
-        //query.selection('price duration etc');
-        const fieldsBy = req.query.fields.split(',').join(' ');
-        query = query.select(fieldsBy);
-      }else {
-        //removing any unnecessary fields not required by any client
-        query = query.select('-__v');
-      }
-
-
-      //5. Pagination
-      const page = Number(req.query.page) || 1; // specifies the page number if multiples pages exist
-      const limit = Number(req.query.limit) || 20; // specified the limit of our results
-      const skip = (page-1) * limit; // skip specifies number of results that are to be skipped to reach the speicified page 
       
-      //url eg: /tours?page=3&limit=10 , page1: 1-10, page2: 11-20, page3: 21-30, skip = 20, so that we react 21-30 result
-      query = query.skip(skip).limit(limit);
-
-      if(req.query.page) {
-        const numTours = await Tour.countDocuments();
-        if(skip > numTours) throw new Error('This page does not exist');
-      }
-
-
       //EXECUTE QUERY : FINAL RESULT
-      //Very importent to resolve the promise at the end so that final result can be obtained
-      const tourData = await query;
+      const features = new ApiFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .pagination();
+        // we have to return this from the previous function to use this kind of chain network
+      const tourData = await features.tourData;
 
       return res.status(200).json({
         success: {  
---
+++ utils/apiFeatures.js
@@ -0,0 +1,72 @@
+
+class ApiFeatures {
+  constructor(tourData, queryString) {
+    this.tourData = tourData;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    // Building Query 
+    // 1. Filtering Query
+    const queryObj = {...this.queryString};
+    const excludedObj = ['page', 'limit', 'sort', 'fields'];
+    excludedObj.forEach(el => delete queryObj[el]);
+
+    //2.  Advanced Filtering
+    //eg: http://localhost:8000/api/v1/tours?duration[gte]=9&difficulty=medium
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    queryStr = JSON.parse(queryStr);
+
+    this.tourData.find(queryStr);
+
+    return this; // this here means send this object instance so that the next function in the chain can access it
+  }
+
+  sort() {
+    // 3. Sort
+    if(this.queryString.sort) {
+      //sorting via multiple parameters in the orders of their appearance
+      //url eg: /tours?sort=price,duration,etc 
+      //query.sort('price duration etc');
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.tourData = this.tourData.sort(sortBy);
+    }else {
+      //default sorting by -ve createdAt parameter so that the newest appears first
+      this.tourData = this.tourData.sort('-createdAt'); 
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    //4. Field Limiting
+    if(this.queryString.fields) {
+      //selecting specific fields from the retrieved data
+      //url eg: /tours?fields=price,duration,etc 
+      //query.selection('price duration etc');
+      const fieldsBy = this.queryString.fields.split(',').join(' ');
+      this.tourData = this.tourData.select(fieldsBy);
+    }else {
+      //removing any unnecessary fields not required by any client
+      this.tourData = this.tourData.select('-__v');
+    }
+
+    return this;
+  }
+
+  pagination() {
+    //5. Pagination
+    const page = Number(this.queryString.page) || 1; // specifies the page number if multiples pages exist
+    const limit = Number(this.queryString.limit) || 20; // specified the limit of our results
+    const skip = (page-1) * limit; // skip specifies number of results that are to be skipped to reach the speicified page 
+    
+    //url eg: /tours?page=3&limit=10 , page1: 1-10, page2: 11-20, page3: 21-30, skip = 20, so that we react 21-30 result
+    this.tourData = this.tourData.skip(skip).limit(limit);
+
+    return this;
+  }
+
+}
+
+module.exports = ApiFeatures;
